64d3fd9be21abcd015ce145ef028f8756484e68f
Martin Chavez <martinchavez1203@gmail.com>
2023-09-27 12:02:53 -0600

    Iteration3

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,28 @@ class Event
       food_truck.inventory.key?(item)
     end
   end
+
+  def overstock_items
+    overstocked_items = []
+
+    item_counts = Hash.new(0)
+
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.each do |item, quantity|
+        item_counts[item] += 1
+      end 
+    end
+  end
+
+  def sorted_item_list
+    unique_names = Set.new
+
+    @food_trucks.each do |food_truck|
+      food_truck.inventory.keys.each do |item|
+        unique_names.add(item.name)
+      end
+    end
+    unique_names.to_a.sort
+  end
+
 end
