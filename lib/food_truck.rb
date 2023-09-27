18894ad190583866d6717fb4c181c3d57a83d501
Martin Chavez <martinchavez1203@gmail.com>
2023-09-27 11:38:17 -0600

    Iteration 2

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -5,11 +5,18 @@ class FoodTruck
     @inventory = Hash.new(0)
   end
 
+  
   def check_stock(item)
     @inventory[item] 
   end
 
+
   def stock(item, quantity)
-    @inventory[item] += quantity  
+    @inventory[item] += quantity
   end
+
+  def potential_revenue
+    @inventory.sum { |item, quantity| item.price * quantity.to_f }
+  end
+
 end
